a84d6cfd46008b2358ad8aa50cf7f448b68e19a8
Hery Santos <hery.silva.santos@gmail.com>
2020-04-13 12:54:43 -0300

    chore(init): implements dockerfile for mysql database
    Also create commands.txt with sequential commands to help start project

---
+++ api/db/script.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS docker_introduction;
+
+USE docker_introduction;
+
+CREATE TABLE IF NOT EXISTS products(
+    id INT(11) AUTO_INCREMENT,
+    name VARCHAR(255),
+    price DECIMAL(10, 2),
+    PRIMARY KEY(id)
+);
+
+INSERT INTO products VALUE (0, 'Curso Front-End', 2500);
+INSERT INTO products VALUE (0, 'Curso Js FullStack', 900);
+INSERT INTO products VALUE (0, 'Curso de Introdução Docker', 1400);
